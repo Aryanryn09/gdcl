a0eeed38423679fe209da433f25d216c5b30e8da
Aryan Bansod <aryanem09@gmail.com>
2023-07-17 22:17:44 +0530

    fe client

---
+++ client/src/TextEditor.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "./textEditor.css";
+import { useQuill } from "react-quilljs";
+import "react-quill/dist/quill.snow.css";
+function TextEditor() {
+  const { quill, quillRef, Quill } = useQuill({ modules: { counter: true } });
+
+  React.useEffect(() => {
+    if (quill) {
+      quill.clipboard.dangerouslyPasteHTML("");
+    }
+  }, [quill]);
+
+  React.useEffect(() => {
+    if (quill) {
+      quill.on("text-change", (delta, oldDelta, source) => {
+        console.log("Text change!");
+        console.log(quill.getText()); // Get text only
+        console.log(quill.getContents()); // Get delta contents
+        console.log(quill.root.innerHTML); // Get innerHTML using quill
+        console.log(quillRef.current.firstChild.innerHTML); // Get innerHTML using quillRef
+      });
+    }
+  }, [quill]);
+
+  const counterRef = React.useRef();
+
+  if (Quill && !quill) {
+    // For execute this line only once.
+    Quill.register("modules/counter", function (quill, options) {
+      quill.on("text-change", function () {
+        const text = quill.getText();
+
+        counterRef.current.innerText = text.split(/\s+/).length;
+      });
+    });
+  }
+
+  return (
+    <div className="textEditor">
+      <div className="cls" style={{ height: "100%" }}>
+        <div ref={quillRef} />
+        <div ref={counterRef} />
+      </div>
+    </div>
+  );
+}
+
+export default TextEditor;
